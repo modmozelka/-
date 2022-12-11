685b4091972b913f17b6ede11ea7198fccc34ed3
Anastasiya <modmozelka@gmail.com>
2022-12-11 18:50:15 +0300

    finish

--- file.py
+++ file.py
@@ -1,30 +1,36 @@
-list_dishes = []
-dishes_1 = []
+cook_book = {}
+
 with open('recipes.txt', 'r', encoding='utf8') as list_of_dishes:
     for line in list_of_dishes:
         dish_name = line.strip()
-        # dishes_1.append(dish_name)
-        dish = {dish_name: []}
+        cook_book[dish_name] = []
         count = list_of_dishes.readline()
         for i in range(int(count)):
             ing = list_of_dishes.readline()
             ingredient_name, quantity, measure = ing.split(' | ')
 
             food = {"ingredient_name": ingredient_name, "quantity": int(quantity), "measure": measure.strip()}
-            dish[dish_name].append(food)
-            # print(food)
+            cook_book[dish_name].append(food)
+
 
         list_of_dishes.readline()
-        list_dishes.append(dish)
-        print(dish)
+# print(cook_book)
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        if dish in cook_book:
+            cook = cook_book[dish]
+            for item in cook:
+                if not item["ingredient_name"] in shop_list:
+                    shop_list[item["ingredient_name"]] = {"quantity" : 0, "measure" : item["measure"]}
+                shop_list[item["ingredient_name"]] ["quantity"] += item["quantity"]*person_count
+        else:
+            raise Exception("В списке блюд нет блюда f'{dish}'")
+    return shop_list
+print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет','Утка по-пекински'], 2))
 
 
-
-
-# print(*list_dishes)
-# from function import get_shop_list_by_dishes
-
-# get_shop_list_by_dishes(['Омлет', 'Утка по-пекински', 'Запеченный картофель'], 2)
 
 
 
